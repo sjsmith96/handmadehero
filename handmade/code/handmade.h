d52d2640aaa45467b9c1740093981f9d85f06660
ClevelandSchemer <samjsmith1996@gmail.com>
2021-12-02 17:53:23 -0500

    Day 063

--- handmade/code/handmade.h
+++ handmade/code/handmade.h
@@ -48,6 +48,7 @@ PushSize_(memory_arena *Arena, memory_index Size)
 #include "handmade_intrinsics.h"
 #include "handmade_math.h"
 #include "handmade_world.h"
+#include "handmade_sim_region.h"
 
 
 struct loaded_bitmap
@@ -67,21 +68,6 @@ struct hero_bitmaps
     loaded_bitmap Torso;
     
     
-};
-
-struct high_entity
-{
-    v2 P; // NOTE: Relative to the camera
-    v2 dP; // velocity
-    uint32 ChunkZ;
-    uint32 FacingDirection;
-
-    real32 tBob;
-
-    real32 Z;
-    real32 dZ;
-
-    uint32 LowEntityIndex;
 };
 
 enum entity_type
@@ -109,13 +95,15 @@ struct low_entity
     entity_type Type;
     
     world_position P;
+    v2 dP;
     real32 Width, Height;
+
+    uint32 FacingDirection;
+    real32 tBob;
 
     // NOTE: This is for "stairs"
     bool32 Collides;
     int32 dAbsTileZ;
-
-    uint32 HighEntityIndex;
 
     // TODO: Should hitpoints themselves be entities?
     uint32 HitPointMax;
@@ -123,13 +111,9 @@ struct low_entity
 
     uint32 SwordLowIndex;
     real32 DistanceRemaining;
-};
 
-struct entity
-{
-    uint32 LowIndex;
-    low_entity *Low;
-    high_entity *High;
+    // TODO: Generation index so we know how up to date this entity
+    // is.
 };
 
 struct entity_visible_piece
@@ -154,13 +138,10 @@ struct game_state
 
     uint32 PlayerIndexForController[ArrayCount(((game_input *)0)->Controllers)];
     
-        
+    // TODO: Change the name to "stored entity"
     uint32 LowEntityCount;
     low_entity LowEntities[100000];
-
-    uint32 HighEntityCount;
-    high_entity HighEntities_[256];
-    
+   
     loaded_bitmap Backdrop;
     loaded_bitmap Shadow;
     
